a921b45a0b08ecfd57e84f68d38e830f2140ea5b
Liko <liko@LikonoMacBook-ea.local>
2019-01-31 08:57:02 +0900

    modify; plugin.xmlを修正、pluginを編集できやすいようにディレクトリを変更

---
+++ dev-plugins/custom-plugin-panorama/src/ios/Vuepanorama.m
@@ -0,0 +1,16 @@
+//
+//  Vuepanorama.m
+//  PanoLiteSwift
+//
+//  Created by Liko on 2019/01/20.
+//  Copyright © 2019 dermandar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RCTBridgeModule.h"
+
+@interface RCT_EXTERN_MODULE(XXX, NSObject)
+
+RCT_EXTERN_METHOD(getAlbums: (RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject)
+
+@end
---
+++ index.js
@@ -0,0 +1,27 @@
+onDeviceReady: function() {
+  app.receivedEvent('deviceready');
+  PanoramicCamera.echo(
+    'Plugin Ready!', 
+    function(msg) { 
+      document
+        .getElementById('deviceready')
+        .querySelector('.received')
+        .innerHTML = msg;   
+    },
+    function(err) {
+      document
+        .getElementById('deviceready')
+        .innerHTML = '<p class="event received">' + err + '</p>'; 
+    }
+  );
+
+  PanoramicCamera.echojs(
+    'Hello Plugin',
+    function(msg) {
+      document.getElementsByTagName('h1')[0].innerHTML = msg;
+    },
+    function(err) {
+      document.getElementsByTagName('h1')[0].innerHTML = err;
+    }
+  );
+}
